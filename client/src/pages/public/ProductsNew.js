31e95d0fb89f0631e147b4fbfdb0b23328aaec48
jtx007alg <13808935+younessaribey@users.noreply.github.com>
2025-11-16 21:18:00 +0100

    fix(shop): remove duplicate Link import

--- client/src/pages/public/ProductsNew.js
+++ client/src/pages/public/ProductsNew.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { Link } from 'react-router-dom';
 import { productsAPI, categoriesAPI } from '../../utils/api';
 import { DEFAULT_PHONE_IMAGE } from '../../data/seedData';
 import {
